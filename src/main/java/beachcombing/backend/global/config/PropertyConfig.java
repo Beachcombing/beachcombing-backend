491a004fc8c36897ed0e39a43b4b75c3cc7ebf5a
Choo <choo000407@naver.com>
2023-07-25 01:00:03 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/beachcombing/backend/global/config/PropertyConfig.java
@@ -0,0 +1,9 @@
+package beachcombing.backend.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:env.properties")
+public class PropertyConfig {
+}
