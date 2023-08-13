9443b450a8d8ba6a557a5e515322f1bdff5d3deb
dltjdn <hana40004@naver.com>
2023-08-14 03:56:28 +0900

    feat: notification domain 생성

---
+++ src/main/java/beachcombing/backend/domain/notification/domain/Notification.java
@@ -0,0 +1,31 @@
+package beachcombing.backend.domain.notification.domain;
+
+import beachcombing.backend.domain.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notification_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    private String title;
+    private String message;
+    private String details;
+}
