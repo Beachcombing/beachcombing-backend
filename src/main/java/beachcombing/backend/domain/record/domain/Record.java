671f413a0a697e85d0251145173bafcb3fe0bea9
Choo <choo000407@naver.com>
2023-08-16 00:48:30 +0900

    feat: Record 도메인 구현

---
+++ src/main/java/beachcombing/backend/domain/record/domain/Record.java
@@ -0,0 +1,58 @@
+package beachcombing.backend.domain.record.domain;
+
+import beachcombing.backend.domain.beach.domain.Beach;
+import beachcombing.backend.domain.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Record {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "record_id")
+    private Long id;
+
+    private LocalTime duration; // 청소 지속 시간 -> 00:00:00 (시, 분, 초)
+    private Long distance; // 청소 거리
+    private String beforeImage; // 청소 전 사진
+    private String afterImage; // 청소 후 사진
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member; // 청소한 사람
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "beach_id")
+    private Beach beach; // 청소한 곳
+
+    @Builder
+    public Record(LocalTime duration, Long distance, String beforeImage, String afterImage, Member member, Beach beach) {
+
+        this.duration =duration;
+        this.distance = distance;
+        this.beforeImage = beforeImage;
+        this.afterImage = afterImage;
+        this.member = member;
+        this.beach = beach;
+    }
+
+    public Record createRecord(LocalTime duration, Long distance, String beforeImage, String afterImage, Member member, Beach beach) {
+
+        return Record.builder()
+                .duration(duration)
+                .distance(distance)
+                .beforeImage(beforeImage)
+                .afterImage(afterImage)
+                .member(member)
+                .beach(beach)
+                .build();
+    }
+}
