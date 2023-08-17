6d7281e5b1c0e1999d8ea5fb8d77e7b0debba226
future430 <future_ii@naver.com>
2023-08-18 00:06:41 +0900

    Feat: Feed, MemberPreferredFeed 도메인 3차수정

--- src/main/java/beachcombing/backend/domain/feed/Feed.java
+++ src/main/java/beachcombing/backend/domain/feed/Feed.java
@@ -1,18 +1,34 @@
 package beachcombing.backend.domain.feed;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Feed {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "feed_id")
     private Long id;
 
-    private String review;
+    private String review; // 리뷰
+
+    @Builder
+    public Feed(String review) {
+        this.review = review;
+    }
+
+    public Feed createFeed(String review)
+    {
+        return Feed.builder()
+                .review(review)
+                .build();
+    }
 
 }
---
+++ src/main/java/beachcombing/backend/domain/member_preferred_feed/MemberPreferredFeed.java
@@ -0,0 +1,43 @@
+package beachcombing.backend.domain.member_preferred_feed;
+
+import beachcombing.backend.domain.feed.Feed;
+import beachcombing.backend.domain.member.domain.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberPreferredFeed {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_preferred_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed; // 좋아요 눌러진 Feed
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member; // 좋아요 누른 회원
+
+    @Builder
+    public MemberPreferredFeed(Feed feed, Member member) {
+        this.feed = feed;
+        this.member = member;
+    }
+
+    public MemberPreferredFeed createMemberPreferredFeed(Feed feed, Member member){
+
+        return MemberPreferredFeed.builder()
+                .feed(feed)
+                .member(member)
+                .build();
+    }
+}
