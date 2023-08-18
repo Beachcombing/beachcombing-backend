a887755f504ae2cd6171f5ac6812adecbb6932c2
dltjdn <hana40004@naver.com>
2023-08-19 05:38:02 +0900

    feat: 생성자에 직접 @Builder 방식으로 변경

--- src/main/java/beachcombing/backend/domain/member/domain/AuthInfo.java
+++ src/main/java/beachcombing/backend/domain/member/domain/AuthInfo.java
@@ -6,11 +6,14 @@ import lombok.*;
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 public class AuthInfo {
     private String loginId; // 로컬 로그인 확장 대비. username 역할
     private String password; // 일반 로그인 위함
     private String provider; // 소셜로그인 구분 위함.
-
+    @Builder
+    public AuthInfo(String loginId, String password, String provider) {
+        this.loginId = loginId;
+        this.password = password;
+        this.provider = provider;
+    }
 }
--- src/main/java/beachcombing/backend/domain/member/domain/Profile.java
+++ src/main/java/beachcombing/backend/domain/member/domain/Profile.java
@@ -7,14 +7,18 @@ import lombok.*;
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Builder
 public class Profile {  // 회원 기본 정보
     private String email;
     @Column(nullable = false, unique = true, length = 20)
     private String nickname;
     private String image; // s3에 저장된 이미지 파일 이름
 
-    //private String role; // USER 혹은 ADMIN
-
+    private String role; // USER 혹은 ADMIN
+    @Builder
+    public Profile(String email, String nickname, String image, String role) {
+        this.email = email;
+        this.nickname = nickname;
+        this.image = image;
+        this.role = role;
+    }
 }
