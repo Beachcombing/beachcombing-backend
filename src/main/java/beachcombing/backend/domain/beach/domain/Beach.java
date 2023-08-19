fd1e6df45b8f0888b785a19acc3d47b33f9914d4
Choo <choo000407@naver.com>
2023-08-20 01:40:34 +0900

    feat: `Record`, `Beach`, `Trashcan` 도메인 구현 (#18)
    * feat: Beach 도메인 구현
    
    * feat: Record 도메인 구현
    
    * docs: Beach 도메인 필드에 대한 주석 추가
    
    * docs: location에 대한 주석 추가
    
    * docs: Record 도메인에 대한 주석 수정
    
    * feat: Trashcan 도메인 구현
    
    * fix: 빠진 static 추가
    
    * style: 띄어쓰기 추가
    
    * fix: column명 오타 수정

---
+++ src/main/java/beachcombing/backend/domain/beach/domain/Beach.java
@@ -0,0 +1,49 @@
+package beachcombing.backend.domain.beach.domain;
+
+import beachcombing.backend.domain.common.domain.Location;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Beach {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "beach_id")
+    private Long id;
+
+    private String name; // 해변 이름
+    private String image; // 해변 사진
+    private String range; // 해변 범위 -> 해변 근처 인증용
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "lat", column = @Column(nullable = false, precision = 10, scale = 8)),
+            @AttributeOverride(name = "lng", column = @Column(nullable = false, precision = 11, scale = 8))
+    })
+    private Location location; // 해변 좌표. TODO: column embeddable 타입 내에서 적용해도 바깥까지 적용되는지 확인해보기
+
+    @Builder
+    public Beach(String name, String image, String range, Location location) {
+
+        this.name = name;
+        this.image = image;
+        this.range = range;
+        this.location = location;
+    }
+
+    public static Beach createBeach(String name, String image, String range, Location location) {
+
+        return Beach.builder()
+                .name(name)
+                .image(image)
+                .range(range)
+                .location(location)
+                .build();
+    }
+}
