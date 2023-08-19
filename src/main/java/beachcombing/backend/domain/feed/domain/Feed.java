98f0baa1912277238d9e1d84d1fe2bc88190cad2
future430 <137784444+future430@users.noreply.github.com>
2023-08-20 01:40:20 +0900

    feat: `Feed`, `MemberPreferredFeed` 도메인 구현 (#14)
    * feat: feed관련 도메인 추가
    
    * Feed, MemberPreferredFeed 도메인 구현 수정
    
    * Revert "Feat: Feed, MemberPreferredFeed 도메인 구현 수정"
    
    This reverts commit 11083a33e372e14fc35c74981b081ca1c52729b3.
    
    * Feat: Feed, MemberPreferredFeed 도메인 3차수정
    
    * Feat: Feed, MemberPreferredFeed 패키지명 수정
    
    ---------
    
    Co-authored-by: Choo <choo000407@naver.com>

---
+++ src/main/java/beachcombing/backend/domain/feed/domain/Feed.java
@@ -0,0 +1,31 @@
+package beachcombing.backend.domain.feed.domain;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Feed {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feed_id")
+    private Long id;
+
+    private String review; // 리뷰
+
+    @Builder
+    public Feed(String review) {
+        this.review = review;
+    }
+
+    public Feed createFeed(String review)
+    {
+        return Feed.builder()
+                .review(review)
+                .build();
+    }
+
+}
