3db95fb13a70e560d90e2a195ba03ecfecf4e0d2
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 19:18:33 +0900

    Feat: 기프트카드 구매 API 구현

--- src/main/java/beachcombing/backend/domain/giftcard/controller/GiftcardController.java
+++ src/main/java/beachcombing/backend/domain/giftcard/controller/GiftcardController.java
@@ -1,12 +1,14 @@
 package beachcombing.backend.domain.giftcard.controller;
 
 import beachcombing.backend.domain.giftcard.dto.GiftcardListResponse;
+import beachcombing.backend.domain.giftcard.dto.PurchaseGiftcardRequest;
+import beachcombing.backend.domain.giftcard.dto.PurchaseGiftcardResponse;
 import beachcombing.backend.domain.giftcard.service.GiftcardService;
+import beachcombing.backend.global.security.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +19,19 @@ public class GiftcardController {
 
     private final GiftcardService giftcardService;
 
-    private
+    // 카드 목록 조회
     @GetMapping("")
-    ResponseEntity<List<GiftcardListResponse>> getGiftcardList() {
+    public ResponseEntity<List<GiftcardListResponse>> getGiftcardList() {
         List<GiftcardListResponse> giftcardListResponses = giftcardService.getGiftcardList();
         return ResponseEntity.ok().body(giftcardListResponses);
+    }
+
+    // 카드 구매
+    @PostMapping("/{giftcardId}/purchase")
+    public ResponseEntity<PurchaseGiftcardResponse> purchaseGiftCard(@PathVariable Long giftcardId, @AuthenticationPrincipal PrincipalDetails userDetails) {
+        PurchaseGiftcardResponse giftcardPurchaseResponse = giftcardService.purchaseGiftcard(PurchaseGiftcardRequest.builder()
+                .memberId(userDetails.getMember().getId()).giftcardId(giftcardId).build());
+
+        return ResponseEntity.ok().body(giftcardPurchaseResponse);
     }
 }
