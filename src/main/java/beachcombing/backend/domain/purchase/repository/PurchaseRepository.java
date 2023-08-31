fd4ed24718932d86774087af89e30f1feb63a1cc
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 19:20:30 +0900

    Feat: PurchaseRepository 구현

--- src/main/java/beachcombing/backend/domain/purchase/repository/PurchaseRepository.java
+++ src/main/java/beachcombing/backend/domain/purchase/repository/PurchaseRepository.java
@@ -1,2 +1,8 @@
-package beachcombing.backend.domain.purchase.repository;public class PurchaseRepository {
+package beachcombing.backend.domain.purchase.repository;
+
+import beachcombing.backend.domain.purchase.domain.Purchase;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
+
 }
