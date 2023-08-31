444b73511ac8f415ef6fc3c01e5e998bdefb05ed
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 17:36:23 +0900

    Feat: GiftcardService 추가 및 giftcard 목록조회 API 구현

--- src/main/java/beachcombing/backend/domain/giftcard/service/GiftcardService.java
+++ src/main/java/beachcombing/backend/domain/giftcard/service/GiftcardService.java
@@ -1,2 +1,29 @@
-package beachcombing.backend.domain.giftcard.service;public class GiftcardService {
+package beachcombing.backend.domain.giftcard.service;
+
+import beachcombing.backend.domain.giftcard.domain.Giftcard;
+import beachcombing.backend.domain.giftcard.dto.GiftcardListResponse;
+import beachcombing.backend.domain.giftcard.repository.GiftcardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class GiftcardService {
+
+    private final GiftcardRepository giftcardRepository;
+
+    public List<GiftcardListResponse> getGiftcardList() {
+        return giftcardRepository.findAll().stream()
+                .map(giftcard -> GiftcardListResponse.builder()
+                        .id(giftcard.getId())
+                        .name(giftcard.getStore().getName())
+                        .location(giftcard.getStore().getLocation())
+                        .image(giftcard.getStore().getImage())
+                        .cost(giftcard.getCost())
+                        .build())
+                .collect(Collectors.toList());
+    }
 }
