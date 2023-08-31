c47712fccebbb8c3fd3071576111b7cc16bed091
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 19:21:35 +0900

    Feat: 잘못된 기프트카드 요청 에러처리 추가

--- src/main/java/beachcombing/backend/global/config/exception/ErrorCode.java
+++ src/main/java/beachcombing/backend/global/config/exception/ErrorCode.java
@@ -13,7 +13,10 @@ public enum ErrorCode {
     // USER 예외
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
     UNAUTHORIZED_ID(HttpStatus.UNAUTHORIZED, "아이디가 틀립니다."),
-    UNAUTHORIZED_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 틀립니다.");
+    UNAUTHORIZED_PASSWORD(HttpStatus.UNAUTHORIZED, "패스워드가 틀립니다."),
+
+    // Giftcard 예외
+    NOT_FOUND_GIFTCARD(HttpStatus.NOT_FOUND, "해당 기프트카드를 찾을 수 없습니다."),;
     private final HttpStatus status;
     private final String error;
 }
