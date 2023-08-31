2e3e1a82641b900c2288a0783a45ee660e816299
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 17:36:49 +0900

    Feat: giftcard 목록조회 API Response 용 DTO 추가

--- src/main/java/beachcombing/backend/domain/giftcard/dto/GiftcardListResponse.java
+++ src/main/java/beachcombing/backend/domain/giftcard/dto/GiftcardListResponse.java
@@ -1,2 +1,15 @@
-package beachcombing.backend.domain.giftcard.dto;public class GiftcardListResponse {
+package beachcombing.backend.domain.giftcard.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+public class GiftcardListResponse {
+    private Long id;
+    private String name;
+    private String location;
+    private String image;
+    private Integer cost;
 }
