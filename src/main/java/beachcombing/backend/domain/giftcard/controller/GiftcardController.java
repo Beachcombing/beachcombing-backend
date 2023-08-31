72ce6e8f5eb4de07b3eb7edd8a436f7cd7d9dc80
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 17:36:08 +0900

    Feat: GiftcardController 추가 및 giftcard 목록조회 API 구현

--- src/main/java/beachcombing/backend/domain/giftcard/controller/GiftcardController.java
+++ src/main/java/beachcombing/backend/domain/giftcard/controller/GiftcardController.java
@@ -1,2 +1,26 @@
-package beachcombing.backend.domain.giftcard.controller;public class GiftcardController {
+package beachcombing.backend.domain.giftcard.controller;
+
+import beachcombing.backend.domain.giftcard.dto.GiftcardListResponse;
+import beachcombing.backend.domain.giftcard.service.GiftcardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/giftcards")
+@RequiredArgsConstructor
+public class GiftcardController {
+
+    private final GiftcardService giftcardService;
+
+    private
+    @GetMapping("")
+    ResponseEntity<List<GiftcardListResponse>> getGiftcardList() {
+        List<GiftcardListResponse> giftcardListResponses = giftcardService.getGiftcardList();
+        return ResponseEntity.ok().body(giftcardListResponses);
+    }
 }
