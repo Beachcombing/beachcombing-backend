918640b60bab35ba2c17faea4fa204543de13440
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 19:19:22 +0900

    Feat: 구매요청DTO 추가

--- src/main/java/beachcombing/backend/domain/giftcard/dto/PurchaseGiftcardRequest.java
+++ src/main/java/beachcombing/backend/domain/giftcard/dto/PurchaseGiftcardRequest.java
@@ -1,2 +1,11 @@
-package beachcombing.backend.domain.giftcard.dto;public class PurchaseGiftcardRequest {
+package beachcombing.backend.domain.giftcard.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PurchaseGiftcardRequest {
+    private Long memberId;
+    private Long giftcardId;
 }
