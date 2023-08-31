764152d404e52f7536b2cbc76b85958be12bf8d4
SungKyum Kim <sungkyum1@naver.com>
2023-08-31 19:20:08 +0900

    Feat: 기프트카드 구매 API 구현

--- src/main/java/beachcombing/backend/domain/giftcard/service/GiftcardService.java
+++ src/main/java/beachcombing/backend/domain/giftcard/service/GiftcardService.java
@@ -2,7 +2,15 @@ package beachcombing.backend.domain.giftcard.service;
 
 import beachcombing.backend.domain.giftcard.domain.Giftcard;
 import beachcombing.backend.domain.giftcard.dto.GiftcardListResponse;
+import beachcombing.backend.domain.giftcard.dto.PurchaseGiftcardRequest;
+import beachcombing.backend.domain.giftcard.dto.PurchaseGiftcardResponse;
 import beachcombing.backend.domain.giftcard.repository.GiftcardRepository;
+import beachcombing.backend.domain.member.domain.Member;
+import beachcombing.backend.domain.member.repository.MemberRepository;
+import beachcombing.backend.domain.purchase.domain.Purchase;
+import beachcombing.backend.domain.purchase.repository.PurchaseRepository;
+import beachcombing.backend.global.config.exception.CustomException;
+import beachcombing.backend.global.config.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +22,8 @@ import java.util.stream.Collectors;
 public class GiftcardService {
 
     private final GiftcardRepository giftcardRepository;
+    private final PurchaseRepository purchaseRepository;
+    private final MemberRepository memberRepository;
 
     public List<GiftcardListResponse> getGiftcardList() {
         return giftcardRepository.findAll().stream()
@@ -25,5 +35,21 @@ public class GiftcardService {
                         .cost(giftcard.getCost())
                         .build())
                 .collect(Collectors.toList());
+    }
+
+    public PurchaseGiftcardResponse purchaseGiftcard(PurchaseGiftcardRequest purchaseGiftcardRequest) {
+        Member member = memberRepository.findById(purchaseGiftcardRequest.getMemberId())
+                .orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_USER));
+        Giftcard giftcard = giftcardRepository.findById(purchaseGiftcardRequest.getGiftcardId())
+                .orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_GIFTCARD));
+
+        Purchase purchase = Purchase.builder()
+                .member(member)
+                .giftcard(giftcard)
+                .build();
+
+        purchaseRepository.save(purchase);
+
+        return PurchaseGiftcardResponse.builder().id(purchase.getId()).build();
     }
 }
