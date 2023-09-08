5557f34e8e2be67deab4e77d8d4ab3d9ff499689
SungKyum Kim <sungkyum1@naver.com>
2023-09-09 01:40:07 +0900

    Refactor: 컨벤션에 맞추어 함수명 변경 및 mapper class 적용

--- src/main/java/beachcombing/backend/domain/giftcard/service/GiftcardService.java
+++ src/main/java/beachcombing/backend/domain/giftcard/service/GiftcardService.java
@@ -2,18 +2,20 @@ package beachcombing.backend.domain.giftcard.service;
 
 import beachcombing.backend.domain.giftcard.domain.Giftcard;
 import beachcombing.backend.domain.giftcard.dto.GiftcardFindAllResponse;
-import beachcombing.backend.domain.giftcard.dto.PurchaseGiftcardRequest;
 import beachcombing.backend.domain.giftcard.dto.PurchaseUpdateResponse;
 import beachcombing.backend.domain.giftcard.dto.PurchaseFindAllResponse;
+import beachcombing.backend.domain.giftcard.mapper.GiftcardMapper;
 import beachcombing.backend.domain.giftcard.repository.GiftcardRepository;
 import beachcombing.backend.domain.member.domain.Member;
 import beachcombing.backend.domain.member.repository.MemberRepository;
 import beachcombing.backend.domain.purchase.domain.Purchase;
+import beachcombing.backend.domain.purchase.mapper.PurchaseMapper;
 import beachcombing.backend.domain.purchase.repository.PurchaseRepository;
 import beachcombing.backend.global.config.exception.CustomException;
 import beachcombing.backend.global.config.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,50 +27,38 @@ public class GiftcardService {
     private final GiftcardRepository giftcardRepository;
     private final PurchaseRepository purchaseRepository;
     private final MemberRepository memberRepository;
+    private final GiftcardMapper giftcardMapper;
+    private final PurchaseMapper purchaseMapper;
 
+    @Transactional(readOnly = true)
     public List<GiftcardFindAllResponse> findAllGiftcard() {
-        List<GiftcardFindAllResponse> giftcardResponseList = giftcardRepository.findAll().stream()
-                .map(giftcard -> GiftcardFindAllResponse.builder()
-                        .id(giftcard.getId())
-                        .name(giftcard.getStore().getName())
-                        .location(giftcard.getStore().getLocation())
-                        .image(giftcard.getStore().getImage())
-                        .cost(giftcard.getCost())
-                        .build())
-                .collect(Collectors.toList());
+        List<Giftcard> giftcardList = giftcardRepository.findAll();
 
-        return giftcardResponseList;
+        return giftcardList.stream()
+                .map(giftcardMapper::toGiftcardFindAllResponse)
+                .toList();
     }
 
-    public PurchaseUpdateResponse updatePurchase(PurchaseGiftcardRequest purchaseGiftcardRequest) {
-        Member member = memberRepository.findById(purchaseGiftcardRequest.getMemberId())
+    public PurchaseUpdateResponse savePurchase(Long memberId, Long giftcardId) {
+        Member member = memberRepository.findById(memberId)
                 .orElseThrow(()-> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
-        Giftcard giftcard = giftcardRepository.findById(purchaseGiftcardRequest.getGiftcardId())
+        Giftcard giftcard = giftcardRepository.findById(giftcardId)
                 .orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_GIFTCARD));
 
-        Purchase purchase = Purchase.builder()
-                .member(member)
-                .giftcard(giftcard)
-                .build();
+        Purchase purchase = Purchase.createPurchase(giftcard, member);
 
         purchaseRepository.save(purchase);
 
-        return PurchaseUpdateResponse.builder().id(purchase.getId()).build();
+        return purchaseMapper.toPurchaseIdResponse(purchase);
     }
 
+    @Transactional(readOnly = true)
     public List<PurchaseFindAllResponse> findAllPurchase(Long memberId) {
-        List<Giftcard> giftcardList = giftcardRepository.findBy(memberId);
 
+        List<Giftcard> giftcardList = purchaseRepository.findByMemberId(memberId).stream().map(Purchase::getGiftcard).toList();
 
-        purchaseRepository.findByMemberId(memberId).stream()
-                .map(Purchase::getGiftcard)
-                .map(giftcard -> PurchaseFindAllResponse.builder()
-                        .id(giftcard.getId())
-                        .name(giftcard.getStore().getName())
-                        .location(giftcard.getStore().getLocation())
-                        .image(giftcard.getStore().getImage())
-                        .cost(giftcard.getCost())
-                        .build())
-                .collect(Collectors.toList());
+        return giftcardList.stream()
+                .map(giftcardMapper::toPurchaseFindAllResponse)
+                .toList();
     }
 }
