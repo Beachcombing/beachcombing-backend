4537fb766c7ea811575dd947ca839da6db58d6bd
SungKyum Kim <sungkyum1@naver.com>
2023-09-09 01:38:33 +0900

    Feat: PurchaseMapper class 구현

--- src/main/java/beachcombing/backend/domain/purchase/mapper/PurchaseMapper.java
+++ src/main/java/beachcombing/backend/domain/purchase/mapper/PurchaseMapper.java
@@ -1,2 +1,25 @@
-package beachcombing.backend.domain.purchase.mapper;public class PurchaseMapper {
+package beachcombing.backend.domain.purchase.mapper;
+
+import beachcombing.backend.domain.giftcard.domain.Giftcard;
+import beachcombing.backend.domain.giftcard.dto.PurchaseUpdateResponse;
+import beachcombing.backend.domain.member.domain.Member;
+import beachcombing.backend.domain.purchase.domain.Purchase;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PurchaseMapper {
+
+    public PurchaseUpdateResponse toPurchaseIdResponse(Purchase purchase) {
+        return PurchaseUpdateResponse.builder()
+                .id(purchase.getId())
+                .build();
+    }
+
+    public Purchase toPurchase(Member member, Giftcard giftcard) {
+        return Purchase.builder()
+                .member(member)
+                .giftcard(giftcard)
+                .build();
+    }
+
 }
