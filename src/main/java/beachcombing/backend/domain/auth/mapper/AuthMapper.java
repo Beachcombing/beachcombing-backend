66b66053481eaaff0d87b3d2717eb45947ce759a
dltjdn <hana40004@naver.com>
2023-09-08 13:27:16 +0900

    fix: AuthMapper Component 등록

--- src/main/java/beachcombing/backend/domain/auth/mapper/AuthMapper.java
+++ src/main/java/beachcombing/backend/domain/auth/mapper/AuthMapper.java
@@ -3,7 +3,11 @@ package beachcombing.backend.domain.auth.mapper;
 import beachcombing.backend.domain.auth.dto.AuthLoginResponse;
 import beachcombing.backend.domain.auth.dto.AuthRefreshResponse;
 import beachcombing.backend.domain.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Component
 public class AuthMapper {
 
     public AuthLoginResponse toAuthLoginResponse(String accessToken,String refreshToken, Member member){
