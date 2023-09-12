20f3b775024ea7c6d668bc38e0e46bebf753d7a3
dltjdn <hana40004@naver.com>
2023-09-13 04:08:59 +0900

    refactor: rayCasting 알고리즘 단순하게 수정

--- src/main/java/beachcombing/backend/domain/beach/service/helper/RayCastingHelper.java
+++ src/main/java/beachcombing/backend/domain/beach/service/helper/RayCastingHelper.java
@@ -10,52 +10,23 @@ import java.util.List;
 public class RayCastingHelper {
     // 점 (x, y)가 다각형 내부에 있는지 판별하는 메서드
     public boolean isInsidePolygon(List<BigDecimal> xCoords, List<BigDecimal> yCoords, BigDecimal x, BigDecimal y) {
+        int numVertices = xCoords.size(); // 다각형의 꼭짓점 수
+        boolean inside = false;
 
-        // 정확도를 위해 double로 계산
-        // 광선은 왼쪽에서 쏜다고 가정
-        int nPoints = xCoords.size();
-        int nIntersections = 0;
+        for (int i = 0, j = numVertices - 1; i < numVertices; j = i++) { // 다각형의 모든 변을 순회
+            BigDecimal xi = xCoords.get(i);
+            BigDecimal yi = yCoords.get(i);
+            BigDecimal xj = xCoords.get(j);
+            BigDecimal yj = yCoords.get(j);
 
-        for (int i = 0; i < nPoints; i++) {
-            BigDecimal x1 = xCoords.get(i);
-            BigDecimal y1 = yCoords.get(i);
-            BigDecimal x2 = xCoords.get((i + 1) % nPoints);
-            BigDecimal y2 = yCoords.get((i + 1) % nPoints);
+            // 현재 점과 현재 선분이 교차하는지를 판별하는 조건
+            boolean intersectCondition = ((yi.compareTo(y) > 0) != (yj.compareTo(y) > 0))
+                    && (x.compareTo(xi.add(xj.subtract(xi).multiply(y.subtract(yi).divide(yj.subtract(yi),RoundingMode.HALF_DOWN)))) < 0);
 
-            BigDecimal minNum = (x1.compareTo(x2)<=0)? x1:x2; // x1<=x2
-            BigDecimal maxNum = (x1.compareTo(x2)<=0)? x2:x1;
-            if(y1.equals(y2) && y.equals(y1) && minNum.compareTo(x)<=0 && maxNum.compareTo(x)>=0){ // minNum <= x <= maxNum
-                return true;
-            }
-            if (isIntersect(x1, y1, x2, y2, x, y)) {
-                nIntersections++;
+            if (intersectCondition) {
+                inside = !inside; // 교차할 때마다 내부/외부 상태를 변경
             }
         }
-        return nIntersections % 2 == 1;
-    }
-
-    // 선분 (x1, y1) -> (x2, y2)와 반직선 y = y0가 교차하는지 판별하는 메서드
-    private boolean isIntersect(BigDecimal x1, BigDecimal y1, BigDecimal x2, BigDecimal y2, BigDecimal x0, BigDecimal y0) {
-
-        // 선분의 y 좌표값이 모두 y0보다 작거나 모두 y0보다 큰 경우 교차하지 않는다.
-        // (y0-y1)*(y0-y2)>0
-        BigDecimal result = (y0.subtract(y1)).multiply(y0.subtract(y2));
-        if (result.compareTo(BigDecimal.ZERO)>0) {
-            return false;
-        }
-
-        // 선분의 기울기가 0일 경우 (y0==y1==y2) --> x값 비교 필요, 함수 호출 전에 이미 검증함
-        if(y1.equals(y2)){
-            return false; // x값 범위 안맞을 때
-        }
-
-
-        // 선분의 기울기가 0이 아닌 경우, 선분의 x 좌표값을 구한다.
-        // (y0 - y1) * (x2 - x1) / (y2 - y1) + x1
-        BigDecimal x =  (((y0.subtract(y1)).multiply(x2.subtract(x1))).divide(y2.subtract(y1), 20, RoundingMode.HALF_UP)).add(x1);
-
-        // x 좌표값이 범위 안에 있는지 확인
-        // x 좌표는 이미 (x-x1)(x-x2)<=0 만족해서 확인필요 없음
-        return x.compareTo(x0) <= 0; // x<=x0
+        return inside;
     }
 }
