92ad934cff7eedd4b7900ec97a1f490089e37d85
dltjdn <hana40004@naver.com>
2023-09-13 04:24:22 +0900

    feat: 액세스 토큰 기간 연장

--- src/main/java/beachcombing/backend/global/security/JwtProperties.java
+++ src/main/java/beachcombing/backend/global/security/JwtProperties.java
@@ -3,7 +3,7 @@ package beachcombing.backend.global.security;
 import java.time.Duration;
 
 public interface JwtProperties {
-    Long ACCESS_TOKEN_EXPIRATION_TIME = Duration.ofMinutes(30).toMillis(); // 30분
+    Long ACCESS_TOKEN_EXPIRATION_TIME = Duration.ofMinutes(120).toMillis(); // 30분
     Long REFRESH_TOKEN_EXPIRATION_TIME = Duration.ofDays(14).toMillis();  // 2주
     String TOKEN_PREFIX = "Bearer ";
     String HEADER_STRING = "Authorization";
