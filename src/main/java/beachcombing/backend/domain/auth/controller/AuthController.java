ed513d57d0a94e3dee36b4c96960cd0fcd6b0576
dltjdn <hana40004@naver.com>
2023-09-18 00:08:03 +0900

    refactor: responseEntity 컨벤션에 맞게 수정

--- src/main/java/beachcombing/backend/domain/auth/controller/AuthController.java
+++ src/main/java/beachcombing/backend/domain/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import beachcombing.backend.domain.auth.controller.dto.AuthRefreshResponse;
 import beachcombing.backend.domain.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +28,7 @@ public class AuthController {
     public ResponseEntity<Void> join(@RequestBody AuthJoinRequest authJoinRequest) {
 
         authService.join(authJoinRequest);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     // 일반 로그인 ( 백엔드 테스트용 )
@@ -35,21 +36,21 @@ public class AuthController {
     public ResponseEntity<AuthLoginResponse> login(@RequestBody AuthLoginRequest request) {
 
         AuthLoginResponse response =authService.login(request);
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     // AccessToken 재발급
     @PostMapping("token")
     public ResponseEntity<AuthRefreshResponse> refresh(@RequestBody Map<String, String> refreshToken){
         AuthRefreshResponse response = authService.refresh(refreshToken.get("refreshToken"));
-        return ResponseEntity.ok().body(response);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     // 로그아웃
     @PostMapping("logout")
     public ResponseEntity<Void> logout(@RequestBody Map<String,String> accessToken){
         authService.logout(accessToken.get("accessToken"));
-        return ResponseEntity.ok().build();
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     //TODO 구글로그인
