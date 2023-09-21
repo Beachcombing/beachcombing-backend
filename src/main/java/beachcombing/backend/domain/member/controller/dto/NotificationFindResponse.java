324e498df223dd30fd58a9fe45bcafeb2401b3f2
dltjdn <hana40004@naver.com>
2023-09-22 02:11:17 +0900

    feat: 알림 목록 조회 service 구현

--- src/main/java/beachcombing/backend/domain/member/controller/dto/NotificationFindResponse.java
+++ src/main/java/beachcombing/backend/domain/member/controller/dto/NotificationFindResponse.java
@@ -1,5 +1,6 @@
 package beachcombing.backend.domain.member.controller.dto;
 
+import beachcombing.backend.domain.notification.domain.Notification;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
@@ -13,4 +14,14 @@ public class NotificationFindResponse {
     public String title;
     public String message;
     public String details;
+
+    public static NotificationFindResponse from(Notification notification){
+        return NotificationFindResponse.builder()
+                .notificationId(notification.getId())
+                .memberId(notification.getMember().getId())
+                .title(notification.getTitle())
+                .message(notification.getMessage())
+                .details(notification.getDetails())
+                .build();
+    }
 }
--- src/main/java/beachcombing/backend/domain/member/service/MemberService.java
+++ src/main/java/beachcombing/backend/domain/member/service/MemberService.java
@@ -7,6 +7,8 @@ import beachcombing.backend.domain.member.controller.dto.MemberFindResponse;
 import beachcombing.backend.domain.member.controller.dto.MemberUpdateRequest;
 import beachcombing.backend.domain.member.mapper.MemberMapper;
 import beachcombing.backend.domain.member.domain.repository.MemberRepository;
+import beachcombing.backend.domain.notification.domain.Notification;
+import beachcombing.backend.domain.notification.repository.NotificationRepository;
 import beachcombing.backend.global.config.exception.CustomException;
 import beachcombing.backend.global.config.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -22,12 +25,13 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final MemberMapper memberMapper;
+    private final NotificationRepository notificationRepository;
 
     //회원 정보 조회
     @Transactional(readOnly = true)
     public MemberFindResponse findMember(long memberId) {
 
-        Member findMember = findMemberById(memberId);
+        Member findMember = getMember(memberId);
         MemberFindResponse response = memberMapper.toMemberFindResponse(findMember);
 
         return response;
@@ -36,7 +40,7 @@ public class MemberService {
     //회원 정보 수정
     public void updateMember(long memberId, MemberUpdateRequest request, Boolean isChanged) {
 
-        Member findMember = findMemberById(memberId);
+        Member findMember = getMember(memberId);
         checkNickname(request.getNickname());
         findMember.getProfile().updateNicknameAndImage(request, isChanged);
 
@@ -55,24 +59,32 @@ public class MemberService {
     //프로필 공개여부 변경하기
     public void updateMemberProfilePublic(Long memberId, Boolean profilePublic) {
 
-        Member findMember = findMemberById(memberId);
+        Member findMember = getMember(memberId);
         findMember.updateProfilePublic(profilePublic);
     }
 
     public void deleteMember(Long memberId) {
-        Member findMember = findMemberById(memberId);
+        Member findMember = getMember(memberId);
         memberRepository.delete(findMember);
     }
 
     public void updateMemberPoint(Long memberId, int option) {
+        Member member = getMember(memberId);
 
     }
-
+    @Transactional(readOnly = true)
     public List<NotificationFindResponse> findNotification(Long memberId) {
+        Member member = getMember(memberId);
+
+        List<NotificationFindResponse> response = notificationRepository.findByMember(member).stream()
+                .map(NotificationFindResponse::from)
+                .toList();
+
+        return response;
     }
 
     //id값으로 멤버 찾기 -> 중복 코드 줄이기
-    private Member findMemberById(long memberId){
+    private Member getMember(long memberId){
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
     }
