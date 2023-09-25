8d58a58a6e2ce2be42dbe52f6c17fb087fc88af7
allllfo <86925185+allllfo@users.noreply.github.com>
2023-09-26 01:38:41 +0900

    feat : 랭킹 조회 API 구현 (#47)
    * Feat: 랭킹 조회 서비스 코드 구현
    
    * chore: QueryDsl 관련 설정
    
    * feat: 랭킹 조회하기 API 구현
    
    * fix: MemberRepositoryCustom extend

--- src/main/java/beachcombing/backend/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/beachcombing/backend/domain/member/domain/repository/MemberRepository.java
@@ -1,14 +1,15 @@
 package beachcombing.backend.domain.member.domain.repository;
 
 import beachcombing.backend.domain.member.domain.Member;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     Optional<Member> findByAuthInfoLoginId(String loginId);
     Boolean existsByProfileNickname(String nickname);
-
-
 
 }
