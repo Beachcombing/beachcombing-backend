51e00e0e5312e0c03795389e5b31ec41c72565af
SungKyum Kim <sungkyum1@naver.com>
2023-09-25 23:35:39 +0900

    Test: findAllFeed API 테스트 추가

--- src/test/java/beachcombing/backend/domain/feed/service/FeedServiceTest.java
+++ src/test/java/beachcombing/backend/domain/feed/service/FeedServiceTest.java
@@ -1,4 +1,34 @@
+package beachcombing.backend.domain.feed.service;
+
+import beachcombing.backend.domain.feed.controller.dto.FeedFindAllResponse;
+import beachcombing.backend.domain.member.domain.Member;
+import beachcombing.backend.domain.member.domain.repository.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class FeedServiceTest {
-  
+
+    @Autowired
+    FeedService feedService;
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    void findAllFeed() {
+        // given
+        Member member = memberRepository.findById(6L).orElseThrow();
+
+        // when
+        List<FeedFindAllResponse> responses = feedService.findAllFeed(member);
+
+        // then
+        Assertions.assertThat(responses.size()).isEqualTo(1);
+    }
 }
