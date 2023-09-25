238f3017208f433a197e934a520086bb0e7461bc
SungKyum Kim <sungkyum1@naver.com>
2023-09-25 23:34:35 +0900

    Feat: MemberPreferredFeedRepository  구현

--- src/main/java/beachcombing/backend/domain/feed/domain/repository/MemberPreferredFeedRepository.java
+++ src/main/java/beachcombing/backend/domain/feed/domain/repository/MemberPreferredFeedRepository.java
@@ -1,2 +1,11 @@
-package beachcombing.backend.domain.feed.domain.repository;public interface MemberPreferredFeedRepository {
+package beachcombing.backend.domain.feed.domain.repository;
+
+import beachcombing.backend.domain.feed.domain.Feed;
+import beachcombing.backend.domain.member.domain.Member;
+import beachcombing.backend.domain.member_preferred_feed.domain.MemberPreferredFeed;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberPreferredFeedRepository extends JpaRepository<MemberPreferredFeed, Long> {
+    Long countByFeed(Feed feed);
+    Boolean existsByMemberAndFeed(Member member, Feed feed);
 }
