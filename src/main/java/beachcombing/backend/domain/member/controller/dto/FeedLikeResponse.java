48be11a851ec41e2f1dc3fc7c21801cc7f70c1f0
SungKyum Kim <sungkyum1@naver.com>
2023-09-26 17:12:40 +0900

    Feat: 피드 좋아요 response 추가

--- src/main/java/beachcombing/backend/domain/member/controller/dto/FeedLikeResponse.java
+++ src/main/java/beachcombing/backend/domain/member/controller/dto/FeedLikeResponse.java
@@ -1,2 +1,20 @@
-package beachcombing.backend.domain.member.controller.dto;public class FeedLikeResponse {
+package beachcombing.backend.domain.member.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class FeedLikeResponse {
+    private Long id;
+
+    public static FeedLikeResponse from(Long id) {
+        return FeedLikeResponse.builder()
+                .id(id)
+                .build();
+    }
 }
