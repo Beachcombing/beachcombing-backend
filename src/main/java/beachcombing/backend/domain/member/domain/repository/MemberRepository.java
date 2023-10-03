351bef001999555f2efc51bc46dd139b470e79a2
dltjdn <78267146+dltjdn@users.noreply.github.com>
2023-10-04 00:53:57 +0900

    Revert "feat : 랭킹 조회 API 구현 (#47)" (#50)
    This reverts commit 8d58a58a6e2ce2be42dbe52f6c17fb087fc88af7.

--- src/main/java/beachcombing/backend/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/beachcombing/backend/domain/member/domain/repository/MemberRepository.java
@@ -1,15 +1,14 @@
 package beachcombing.backend.domain.member.domain.repository;
 
 import beachcombing.backend.domain.member.domain.Member;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByAuthInfoLoginId(String loginId);
     Boolean existsByProfileNickname(String nickname);
+
+
 
 }
