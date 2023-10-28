a980fe87b95d18ca39ea5f6270075540e1d2273b
Choo <choo000407@naver.com>
2023-10-28 18:58:40 +0900

    feat: 역 지오코딩 로직 추가

--- src/main/java/beachcombing/backend/global/init/GeocodingHelper.java
+++ src/main/java/beachcombing/backend/global/init/GeocodingHelper.java
@@ -1,17 +1,17 @@
 package beachcombing.backend.global.init;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Component
 public class GeocodingHelper {
@@ -19,10 +19,14 @@ public class GeocodingHelper {
     @Value("${GCP_API_KEY}")
     private String API_KEY;
 
-    public Map<String, String> getGeoDataByAddress(String completeAddress) {
+    // 지오코딩
+    public Map<String, String> getCoordsByAddress(String completeAddress) {
+
         try {
-            String surl = "https://maps.googleapis.com/maps/api/geocode/json?address="+ URLEncoder.encode(completeAddress, "UTF-8")+
-                    "&key="+URLEncoder.encode(API_KEY, "UTF-8");
+            String surl =
+                    "https://maps.googleapis.com/maps/api/geocode/json?address=" + URLEncoder.encode(completeAddress,
+                            "UTF-8") +
+                            "&key=" + URLEncoder.encode(API_KEY, "UTF-8");
             URL url = new URL(surl);
             InputStream is = url.openConnection().getInputStream();
 
@@ -30,30 +34,64 @@ public class GeocodingHelper {
 
             StringBuilder responseStrBuilder = new StringBuilder();
             String inputStr;
-            // System.out.println(">>>>>>>>>> >>>>>>>>>> InputStream Start <<<<<<<<<< <<<<<<<<<<");
+            //System.out.println(">>>>>>>>>> >>>>>>>>>> InputStream Start <<<<<<<<<< <<<<<<<<<<");
             while ((inputStr = streamReader.readLine()) != null) {
-                // System.out.println(">>>>>>>>>>     "+inputStr);
+                //System.out.println(">>>>>>>>>>     "+inputStr);
                 responseStrBuilder.append(inputStr);
             }
-            // System.out.println(">>>>>>>>>> >>>>>>>>>> InputStream End <<<<<<<<<< <<<<<<<<<<");
+            //System.out.println(">>>>>>>>>> >>>>>>>>>> InputStream End <<<<<<<<<< <<<<<<<<<<");
 
             JSONObject jo = new JSONObject(responseStrBuilder.toString());
             JSONArray results = jo.getJSONArray("results");
-            String region = null;
-            String province = null;
-            String zip = null;
             Map<String, String> ret = new HashMap<String, String>();
-            if(results.length() > 0) {
-                JSONObject jsonObject;
-                jsonObject = results.getJSONObject(0);
+            if (results.length() > 0) {
+                JSONObject jsonObject = results.getJSONObject(0);
                 Double lat = jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lat");
                 Double lng = jsonObject.getJSONObject("geometry").getJSONObject("location").getDouble("lng");
                 ret.put("lat", lat.toString());
                 ret.put("lng", lng.toString());
-                // System.out.println("LAT:\t\t"+lat);
-                // System.out.println("LNG:\t\t"+lng);
+                //System.out.println("LAT:\t\t"+lat);
+                //System.out.println("LNG:\t\t"+lng);
 
                 return ret;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 역 지오코딩
+    public String getAddressByCoords(BigDecimal lat, BigDecimal lng) {
+
+        String language = "ko"; // 영어:en, 한국어:ko
+        try {
+            String surl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng +
+                    "&key=" + URLEncoder.encode(API_KEY, "UTF-8") +
+                    "&language=" + language +
+                    "&result_type=street_address";
+            URL url = new URL(surl);
+            InputStream is = url.openConnection().getInputStream();
+
+            BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            StringBuilder responseStrBuilder = new StringBuilder();
+            String inputStr;
+            //System.out.println(">>>>>>>>>> >>>>>>>>>> InputStream Start <<<<<<<<<< <<<<<<<<<<");
+            while ((inputStr = streamReader.readLine()) != null) {
+                //System.out.println(">>>>>>>>>>     "+inputStr);
+                responseStrBuilder.append(inputStr);
+            }
+            //System.out.println(">>>>>>>>>> >>>>>>>>>> InputStream End <<<<<<<<<< <<<<<<<<<<");
+
+            JSONObject jo = new JSONObject(responseStrBuilder.toString());
+            JSONArray results = jo.getJSONArray("results");
+            if (results.length() > 0) {
+                JSONObject jsonObject = results.getJSONObject(0);
+                String address = jsonObject.getString("formatted_address");
+                //System.out.println("ADDRESS:\t\t"+address);
+
+                return address;
             }
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/beachcombing/backend/global/init/InitService.java
+++ src/main/java/beachcombing/backend/global/init/InitService.java
@@ -3,22 +3,21 @@ package beachcombing.backend.global.init;
 import beachcombing.backend.domain.common.domain.Location;
 import beachcombing.backend.domain.trashcan.domain.Trashcan;
 import beachcombing.backend.domain.trashcan.domain.repository.TrashcanRepository;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.math.BigDecimal;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Map;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
@@ -41,8 +40,8 @@ public class InitService {
 
         String urlStr = "https://api.odcloud.kr/api/15089095/v1/uddi:c03ea19d-e9b0-4cfe-9714-96a9bb363db6?" +
                 "page=" + page +
-                "&perPage=" + perPage+
-                "&serviceKey="+serviceKey;
+                "&perPage=" + perPage +
+                "&serviceKey=" + serviceKey;
         StringBuffer result = new StringBuffer();
 
         try {
@@ -58,16 +57,16 @@ public class InitService {
             JSONObject jsonObject = (JSONObject) jsonParser.parse(result.toString());
             JSONArray jsonArray = (JSONArray) jsonObject.get("data");
 
-            for(int i=0; i<jsonArray.size(); i++) {
+            for (int i = 0; i < jsonArray.size(); i++) {
                 JSONObject object = (JSONObject) jsonArray.get(i);
                 String address = (String) object.get("설치장소");
-                Map<String, String> coords = geocodingHelper.getGeoDataByAddress(address);
+                Map<String, String> coords = geocodingHelper.getCoordsByAddress(address);
 
                 Location location = Location.builder()
                         .lat(new BigDecimal(coords.get("lat")))
                         .lng(new BigDecimal(coords.get("lng")))
                         .build();
-                Trashcan trashcan = Trashcan.createTrashcanByApi(location);
+                Trashcan trashcan = Trashcan.createTrashcanByApi(location, address);
                 trashcanRepository.save(trashcan);
             }
 
