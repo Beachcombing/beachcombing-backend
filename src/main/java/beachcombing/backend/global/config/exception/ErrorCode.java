a73950873c53f19ba020089936c98c6db2c964df
Choo <choo000407@naver.com>
2023-10-28 17:56:29 +0900

    feat: 에러메시지 추가

--- src/main/java/beachcombing/backend/global/config/exception/ErrorCode.java
+++ src/main/java/beachcombing/backend/global/config/exception/ErrorCode.java
@@ -25,11 +25,11 @@ public enum ErrorCode {
     // Token 예외
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "토큰이 유효하지 않습니다."),
-    UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "인증되지 않은 토큰입니다." ),
+    UNAUTHORIZED_TOKEN(HttpStatus.UNAUTHORIZED, "인증되지 않은 토큰입니다."),
 
     // Record 애외
     NOT_FOUND_RECORD(HttpStatus.NOT_FOUND, "해당 기록을 찾을 수 없습니다."),
-    
+
     // Giftcard 예외
     NOT_FOUND_GIFTCARD(HttpStatus.NOT_FOUND, "해당 기프트카드를 찾을 수 없습니다."),
 
@@ -42,11 +42,12 @@ public enum ErrorCode {
     PERMISSION_DENIED(HttpStatus.UNAUTHORIZED, "타인의 글은 수정 및 삭제할 수 없습니다."),
     EXIST_FEED_RECORD(HttpStatus.CONFLICT, "이미 피드가 작성된 레코드입니다."),
 
+    // Trashcan 예외
+    NOT_FOUND_TRASHCAN(HttpStatus.NOT_FOUND, "해당 쓰레기통을 찾을 수 없습니다."),
+
     // 이미지 업로드 예외
     UPLOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3 파일 업로드에 실패했습니다."),
-
     ;
-
 
     private final HttpStatus status;
     private final String error;
